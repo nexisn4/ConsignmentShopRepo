b300310fac64157df09bad6f73bba100824cef20
Youchan Kim <yckim@ucdavis.edu>
2019-04-23 19:26:30 -0700

    finished SetupData()

--- ConsignmentShop/ConsignmentShopUI/ConsignmentShop.cs
+++ ConsignmentShop/ConsignmentShopUI/ConsignmentShop.cs
@@ -23,17 +23,54 @@ namespace ConsignmentShopUI
 
         private void SetupData()
         {
-            //Vendor demoVendor = new Vendor();
-            //demoVendor.FirstName = "Bill";
-            //demoVendor.LastName = "Smith";
-            //demoVendor.Commission = 0.5;
-            //store.Vendors.Add(demoVendor);
-
+            //add vendors
             store.Vendors.Add(new Vendor
             {
                 FirstName = "Bill",
                 LastName = "Smith",
             });
+
+            store.Vendors.Add(new Vendor
+            {
+                FirstName = "Sue",
+                LastName = "Jones",
+            });
+
+            //add items
+            store.Items.Add(new Item
+            {
+                Title = "Moby Dick",
+                Description = "A book about whale",
+                Price = 4.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "A Tale of Two Cities",
+                Description = "A book about a revolution",
+                Price = 3.80M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Harry Potter 1",
+                Description = "A book about wizards",
+                Price = 5.20M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Jane Eyre",
+                Description = "A book about a girl",
+                Price = 1.50M,
+                Owner = store.Vendors[0]
+            });
+
+            //set store
+            store.Name = "Seconds are Better";
         }
 
     }
